3326c91730430e6e4967e9349e7a90e5d02a5020
ipotrader22-cloud <ipotrader22@gmail.com>
2026-09-09 16:44:11 -0400

    Remove temporary dashboard DOM diagnostic

--- tests/diagnose_dashboard_snapshot_follow_up.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const fs = require('fs');
-const lines = fs.readFileSync(require.resolve('../app.js'), 'utf8').split(/\r?\n/);
-for (const [startLine, endLine, title] of [[5340, 5390, 'Dashboard data builder entry'], [13990, 14082, 'Dashboard route']]) {
-  console.log(`\n===== ${title} =====`);
-  const start = startLine - 1;
-  console.log(lines.slice(start, endLine).map((line, index) => `${start + index + 1}: ${line}`).join('\n'));
-}
